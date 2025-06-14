cd8385c1434552d124b2ff095775d10654e56fc0
Filipp Vakhitov <filipp.vakhitov@jetbrains.com>
2025-06-14 14:44:11 +0300

    Fix reaction routes

--- src/main/kotlin/fi/lipp/blog/routes/ReactionRoutes.kt
+++ src/main/kotlin/fi/lipp/blog/routes/ReactionRoutes.kt
@@ -2,12 +2,10 @@ package fi.lipp.blog.routes
 
 import fi.lipp.blog.data.toFileUploadDatas
 import fi.lipp.blog.plugins.userId
+import fi.lipp.blog.plugins.viewer
 import fi.lipp.blog.service.ReactionService
-import fi.lipp.blog.service.Viewer
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
-import io.ktor.server.plugins.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -62,51 +60,41 @@ fun Route.reactionRoutes(reactionService: ReactionService) {
             }
         }
 
-        post("/post-reaction") {
-            val diaryLogin = call.request.queryParameters["login"] ?: throw IllegalArgumentException("Missing diaryLogin parameter")
-            val uri = call.request.queryParameters["uri"] ?: throw IllegalArgumentException("Missing uri parameter")
-            val reactionName = call.request.queryParameters["name"] ?: throw IllegalArgumentException("Missing reactionName parameter")
+        authenticate(optional = true) {
+            post("/post-reaction") {
+                val diaryLogin = call.request.queryParameters["login"] ?: throw IllegalArgumentException("Missing diaryLogin parameter")
+                val uri = call.request.queryParameters["uri"] ?: throw IllegalArgumentException("Missing uri parameter")
+                val reactionName = call.request.queryParameters["name"] ?: throw IllegalArgumentException("Missing reactionName parameter")
 
-            val viewer = call.principal<JWTPrincipal>()?.let { Viewer.Registered(userId) }
-                ?: Viewer.Anonymous(call.request.local.remoteHost, call.request.headers["User-Agent"] ?: "unknown")
+                reactionService.addReaction(viewer, diaryLogin, uri, reactionName)
+                call.respondText("Reaction added successfully")
+            }
 
-            reactionService.addReaction(viewer, diaryLogin, uri, reactionName)
-            call.respondText("Reaction added successfully")
-        }
+            delete("/post-reaction") {
+                val diaryLogin = call.request.queryParameters["login"] ?: throw IllegalArgumentException("Missing diaryLogin parameter")
+                val uri = call.request.queryParameters["uri"] ?: throw IllegalArgumentException("Missing uri parameter")
+                val reactionName = call.request.queryParameters["name"] ?: throw IllegalArgumentException("Missing reactionName parameter")
 
-        delete("/post-reaction") {
-            val diaryLogin = call.request.queryParameters["login"] ?: throw IllegalArgumentException("Missing diaryLogin parameter")
-            val uri = call.request.queryParameters["uri"] ?: throw IllegalArgumentException("Missing uri parameter")
-            val reactionName = call.request.queryParameters["name"] ?: throw IllegalArgumentException("Missing reactionName parameter")
+                reactionService.removeReaction(viewer, diaryLogin, uri, reactionName)
+                call.respondText("Reaction removed successfully")
+            }
 
-            val viewer = call.principal<JWTPrincipal>()?.let { Viewer.Registered(userId) }
-                ?: Viewer.Anonymous(call.request.local.remoteHost, call.request.headers["User-Agent"] ?: "unknown")
+            // Comment reactions
+            post("comment-reaction") {
+                val commentId = call.request.queryParameters["commentId"]?.let { UUID.fromString(it) } ?: throw IllegalArgumentException("Missing commentId parameter")
+                val reactionName = call.request.queryParameters["name"] ?: throw IllegalArgumentException("Missing reactionName parameter")
 
-            reactionService.removeReaction(viewer, diaryLogin, uri, reactionName)
-            call.respondText("Reaction removed successfully")
-        }
+                reactionService.addCommentReaction(viewer, commentId, reactionName)
+                call.respondText("Comment reaction added successfully")
+            }
 
-        // Comment reactions
-        post("comment-reaction") {
-            val commentId = call.request.queryParameters["commentId"]?.let { UUID.fromString(it) } ?: throw IllegalArgumentException("Missing commentId parameter")
-            val reactionName = call.request.queryParameters["name"] ?: throw IllegalArgumentException("Missing reactionName parameter")
+            delete("comment-reaction") {
+                val commentId = call.request.queryParameters["commentId"]?.let { UUID.fromString(it) } ?: throw IllegalArgumentException("Missing commentId parameter")
+                val reactionName = call.request.queryParameters["name"] ?: throw IllegalArgumentException("Missing reactionName parameter")
 
-            val viewer = call.principal<JWTPrincipal>()?.let { Viewer.Registered(userId) }
-                ?: Viewer.Anonymous(call.request.origin.remoteHost, call.request.headers["User-Agent"] ?: "unknown")
-
-            reactionService.addCommentReaction(viewer, commentId, reactionName)
-            call.respondText("Comment reaction added successfully")
-        }
-
-        delete("comment-reaction") {
-            val commentId = call.request.queryParameters["commentId"]?.let { UUID.fromString(it) } ?: throw IllegalArgumentException("Missing commentId parameter")
-            val reactionName = call.request.queryParameters["name"] ?: throw IllegalArgumentException("Missing reactionName parameter")
-
-            val viewer = call.principal<JWTPrincipal>()?.let { Viewer.Registered(userId) }
-                ?: Viewer.Anonymous(call.request.origin.remoteHost, call.request.headers["User-Agent"] ?: "unknown")
-
-            reactionService.removeCommentReaction(viewer, commentId, reactionName)
-            call.respondText("Comment reaction removed successfully")
+                reactionService.removeCommentReaction(viewer, commentId, reactionName)
+                call.respondText("Comment reaction removed successfully")
+            }
         }
     }
 }
